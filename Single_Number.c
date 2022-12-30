ca729239e277a7d3b81bbe274d0bffbf28415c3b
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2022-12-30 18:36:56 +0530

    #include<stdio.h> int main() {     int n,i,j,q,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<=n;i++)     {         c=0;         for(j=0,q=n;j<q;j++)         {             if(i!=j)             {                 if(a[i]==a[j])                 {                     c=c+1;                 }             }         }         if(c==0)         {             printf("%d ",a[i]);break;         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,q,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n;i++)
+    {
+        c=0;
+        for(j=0,q=n;j<q;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                {
+                    c=c+1;
+                }
+            }
+        }
+        if(c==0)
+        {
+            printf("%d ",a[i]);break;
+        }
+    }
+}

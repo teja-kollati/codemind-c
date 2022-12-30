095daa1fcd073b6eb1ae011538f1dc6e4a234176
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2022-12-30 17:55:50 +0530

    #include<stdio.h> int main() {     int n;     while(1)     {         scanf("%d",&n);         if(n==-1)         {             break;         }         else         {             printf("%d ",n*n);         }     } }

---
+++ Infinite.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    while(1)
+    {
+        scanf("%d",&n);
+        if(n==-1)
+        {
+            break;
+        }
+        else
+        {
+            printf("%d
+",n*n);
+        }
+    }
+}

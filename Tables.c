ce1d3e271ee8ba548988d20fd6361314f91f54e1
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2023-01-14 18:21:27 +0530

    #include<stdio.h> int main() {     int a,b,p,i;     scanf("%d%d",&a,&b);     for(i=1;i<=b;i++)     {         if(i%2!=0)         {             p=a*i;             printf ("%d x %d = %d ",a,i,p);         }     } }

---
+++ Tables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,p,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        if(i%2!=0)
+        {
+            p=a*i;
+            printf ("%d x %d = %d
+",a,i,p);
+        }
+    }
+}

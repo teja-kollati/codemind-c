ac9241d69be50a7a0f346fa9db2f544428db66d7
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2023-01-19 08:55:55 +0530

    #include<stdio.h> int main() {     int n,r,s,a;     scanf("%d",&n);     a=n;     while(a>0)     {         s=0;         while(a>0)         {             r=a%10;             s=s+r*r;             a=a/10;         }         if(s>9)         {             a=s;         }     }     if(s==1 || s==7)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,s,a;
+    scanf("%d",&n);
+    a=n;
+    while(a>0)
+    {
+        s=0;
+        while(a>0)
+        {
+            r=a%10;
+            s=s+r*r;
+            a=a/10;
+        }
+        if(s>9)
+        {
+            a=s;
+        }
+    }
+    if(s==1 || s==7)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}

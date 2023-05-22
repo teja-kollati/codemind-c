f84c0c8769d939876afe16add58a43c4867ebe4e
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2023-05-22 20:03:42 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int dig_cnt[10] = {0};     int dig;     while(n > 0){         dig  = n % 10;         dig_cnt[dig]++;         n /= 10;     }     int i,c = 0;     for (i = 0; i < 10; i ++){         if(dig_cnt[i] > 1){             c = 0;             break;         }         else{             c = 1;         }     }     if(c == 0) printf("Not Unique Number");     else printf("Unique Number"); }

--- Unique_number.c
+++ Unique_number.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
-int main()
-{
-    char str[20];
-    scanf("%s",str);
-    int i,j,temp,count=0;
-    for(i=0;str[i]!=NULL;i++)
-    {
-        temp=str[i];
-        for(j=(i+1);str[j]!=NULL;j++)
-        {
-            if(str[j]==temp)
-            {
-                count++;
-            }
+int main(){
+    int n;
+    scanf("%d",&n);
+    int dig_cnt[10] = {0};
+    int dig;
+    while(n > 0){
+        dig  = n % 10;
+        dig_cnt[dig]++;
+        n /= 10;
+    }
+    int i,c = 0;
+    for (i = 0; i < 10; i ++){
+        if(dig_cnt[i] > 1){
+            c = 0;
+            break;
+        }
+        else{
+            c = 1;
         }
     }
-    if(count==0)
-    {
-        printf("Unique Number");
-    }
-    else
-    {
-        printf("Not Unique Number");
-    }
+    if(c == 0) printf("Not Unique Number");
+    else printf("Unique Number");
 }

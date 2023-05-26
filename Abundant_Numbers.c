ac8e9139c34c2c9a0d75070e64392cf3f0b05da0
teja-kollati <119738827+teja-kollati@users.noreply.github.com>
2023-05-26 10:50:47 +0530

    #include<stdio.h> int SumOfFactors(int n){     int sum = 0;     for(int i = 1; i < n; i++){         if(n % i == 0){             sum += i;         }     }     return sum; } int Isabundant(int num){     int sum = SumOfFactors(num);     if(sum > num) return 1;     else return -1; } int main(){     int num;     scanf("%d",&num);     if(Isabundant(num) == 1) printf("True");     else printf("False"); }

--- Abundant_Numbers.c
+++ Abundant_Numbers.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
-int main()
-{
-    int n,s=0,i;
-    scanf("%d",&n);
-    for(i=1;i<n;i++) 
-    {
-        if(n%i==0)
-        s=s+i;
+int SumOfFactors(int n){
+    int sum = 0;
+    for(int i = 1; i < n; i++){
+        if(n % i == 0){
+            sum += i;
+        }
     }
-    if(s>n)
-    printf("True");
-    else
-    printf("False");
+    return sum;
+}
+int Isabundant(int num){
+    int sum = SumOfFactors(num);
+    if(sum > num) return 1;
+    else return -1;
+}
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(Isabundant(num) == 1) printf("True");
+    else printf("False");
 }

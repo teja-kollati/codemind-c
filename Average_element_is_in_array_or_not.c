01b62a6ec09caba1916a9608a3a2acec60a49459
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-06-20 17:58:10 +0530

    #include<stdio.h> int Binary_search(int *arr, int n, int se , int low , int high){ 	int mid; 	if (low > high){ 		return -1; 	} 	else{ 		mid = (low + high)/2; 		if(arr[mid] == se) 		return mid; 		else if (arr[mid] < se) 		return Binary_search(arr, n, se, mid + 1, high); 		else if (arr[mid] > se) 		return Binary_search(arr, n, se, low, mid - 1); 	} } int main(){ 	int n, i; 	scanf("%d",&n); 	int arr[n]; 	for(i = 0; i < n; i++){ 		scanf("%d",&arr[i]); 	} 	int sum = 0; 	for(int i = 0; i < n; i++){ 	    sum += arr[i]; 	} 	int se = sum / n; 	int low = 0, high = n-1; 	int res = Binary_search(arr,n,se,low,high); 	if (res == -1) 	printf("False"); 	else 	printf("True"); }

---
+++ Average_element_is_in_array_or_not.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int Binary_search(int *arr, int n, int se , int low , int high){
+	int mid;
+	if (low > high){
+		return -1;
+	}
+	else{
+		mid = (low + high)/2;
+		if(arr[mid] == se)
+		return mid;
+		else if (arr[mid] < se)
+		return Binary_search(arr, n, se, mid + 1, high);
+		else if (arr[mid] > se)
+		return Binary_search(arr, n, se, low, mid - 1);
+	}
+}
+int main(){
+	int n, i;
+	scanf("%d",&n);
+	int arr[n];
+	for(i = 0; i < n; i++){
+		scanf("%d",&arr[i]);
+	}
+	int sum = 0;
+	for(int i = 0; i < n; i++){
+	    sum += arr[i];
+	}
+	int se = sum / n;
+	int low = 0, high = n-1;
+	int res = Binary_search(arr,n,se,low,high);
+	if (res == -1)
+	printf("False");
+	else
+	printf("True");
+}

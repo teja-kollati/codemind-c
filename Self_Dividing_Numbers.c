ecdae70b054ec8ca330778abf2651ccd532017f5
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-06-20 08:49:45 +0530

    #include<stdio.h> int main(){     int a,b;     scanf("%d%d",&a,&b);     for(int i = a; i <= b; i++){         int temp = i;         int len = 0;         int count = 0;         while(temp > 0){             int r = temp % 10;             len++;             if(r != 0){                 if(i % r == 0){                     count++;                 }             }             temp /= 10;         }         if(len == count){             printf("%d ",i);         }     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
-int main()
-{
-    int a,b,c,i,j,k=0,m=0;
-    scanf("%d %d",&a,&b);
-    for(i=a;i<=b;i++)
-    {
-        m=0;
-        k=0;
-        for(j=i;j>0;j=j/10)
-        {
-            c=j%10;
-            m++;
-            if(c!=0)
-            {
-                if(i%c==0)
-                {
-                    k++;
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i = a; i <= b; i++){
+        int temp = i;
+        int len = 0; 
+        int count = 0;
+        while(temp > 0){
+            int r = temp % 10;
+            len++;
+            if(r != 0){
+                if(i % r == 0){
+                    count++;
                 }
             }
+            temp /= 10;
         }
-        if(k==m)
-        {
+        if(len == count){
             printf("%d ",i);
         }
     }

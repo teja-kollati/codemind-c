735809eb6de49d938a6feb18fa019a7741fcd062
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-06-25 09:44:50 +0530

    #include<stdio.h> int main(){     int n,m;     scanf("%d%d",&n,&m);     int mat[n][m];     for(int i = 0; i < n; i++){         for(int j = 0; j < m; j++){             scanf("%d",&mat[i][j]);         }     }     int sum = 0;     for(int i = 0; i < n; i++){         for(int j = 0; j < m; j++){             sum += mat[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int mat[n][m];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            sum += mat[i][j];
+        }
+    }
+    printf("%d",sum);
+}

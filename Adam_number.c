4dd17a396a8f1d640956857145d3d0b22d3fd311
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-10 13:45:49 +0530

    #include <stdio.h> #include <math.h> int reverse(int num) {     int rev = 0;     while (num > 0) {         rev = rev * 10 + num % 10;         num /= 10;     }     return rev; } int isAdamNumber(int num) {     int originalSquare = num * num;     int reverseSquare = reverse(num) * reverse(num);     if(originalSquare == reverse(reverseSquare) == 1){         return 1;     }     else{         return 0;     } }
    int main() {
        int n;
        scanf("%d", &n);
        if (isAdamNumber(n) == 1)
            printf("True");
        else
            printf("False");
    }

--- Adam_number.c
+++ Adam_number.c
@@ -1,33 +1,29 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int n,q,sn1,sn2,r,rsn2,s=0;
-    scanf("%d",&n);
-    sn1=n*n;
-    q=n;
-    while(q!=0)
-    {
-        r=q%10;
-        q=q/10;
-        s=s*10+r;
+#include <stdio.h>
+#include <math.h>
+int reverse(int num) {
+    int rev = 0;
+    while (num > 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
     }
-    sn2=s*s;
-    q=sn2;
-    s=0;
-    while(q!=0)
-    {
-        r=q%10;
-        q=q/10;
-        s=s*10+r;
+    return rev;
+}
+int isAdamNumber(int num) {
+    int originalSquare = num * num;
+    int reverseSquare = reverse(num) * reverse(num);
+    if(originalSquare == reverse(reverseSquare) == 1){
+        return 1;
     }
-    rsn2=s;
-    if(sn1==rsn2)
-    {
+    else{
+        return 0;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (isAdamNumber(n) == 1)
         printf("True");
-    }
     else
-    {
         printf("False");
-    }
 }

51b27597633bb2d9c573d811cb341062ad60e330
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-11 18:02:51 +0530

    #include<stdio.h> int main(){     char str[50];     scanf("%[^ ]",str);     int count = 0;     for(int i = 0; str[i] != NULL; i++){         if(str[i] >= 'A' && str[i] <= 'Z'){             count++;         }     }     printf("%d",count); }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[50];
+    scanf("%[^
+]",str);
+    int count = 0;
+    for(int i = 0; str[i] != NULL; i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            count++;
+        }
+    }
+    printf("%d",count);
+}

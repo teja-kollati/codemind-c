948caa93bc327a8319ad2266d59bd09885411604
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-11 17:01:04 +0530

    #include<stdio.h> int max(int *sum, int n){     int max = 0;     for(int i = 0; i < n; i++){         if(max < sum[i]){             max = sum[i];         }     }     return max; } int main(){     int n,m;     scanf("%d%d",&n,&m);     int mat[n][m];     for(int i = 0; i < n; i++){         for(int j = 0; j < m;j++){             scanf("%d",&mat[i][j]);         }     }     int rowsum[n];     for(int i = 0; i < n; i++){         rowsum[i] = 0;         for(int j = 0; j < m; j++){             rowsum[i] += mat[i][j];         }     }     int colsum[m];     for(int i = 0; i < m; i++){         colsum[i] = 0;         for(int j = 0; j < n; j++){             colsum[i] += mat[j][i];         }     }     if(max(rowsum,n) > max(colsum,m)){         printf("%d",max(rowsum,n));     }     else{         printf("%d",max(colsum,m));     } }

---
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int max(int *sum, int n){
+    int max = 0;
+    for(int i = 0; i < n; i++){
+        if(max < sum[i]){
+            max = sum[i];
+        }    
+    }
+    return max;
+}
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int mat[n][m];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m;j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    int rowsum[n];
+    for(int i = 0; i < n; i++){
+        rowsum[i] = 0;
+        for(int j = 0; j < m; j++){
+            rowsum[i] += mat[i][j];
+        }
+    }
+    int colsum[m];
+    for(int i = 0; i < m; i++){
+        colsum[i] = 0;
+        for(int j = 0; j < n; j++){
+            colsum[i] += mat[j][i];
+        }
+    }
+    if(max(rowsum,n) > max(colsum,m)){
+        printf("%d",max(rowsum,n));
+    }
+    else{
+        printf("%d",max(colsum,m));
+    }
+}

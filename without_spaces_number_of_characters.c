0db5974f1d13a2816bb5f88c230310ceec8d7220
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-11 17:51:34 +0530

    #include<stdio.h> #include<string.h> int main(){     char str[50];     scanf("%[^ ]",str);     int count = 0;     for(int i = 0; str[i] != NULL; i++){         if(str[i] != ' '){             count++;         }     }     printf("%d",count); }

---
+++ without_spaces_number_of_characters.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    scanf("%[^
+]",str);
+    int count = 0;
+    for(int i = 0; str[i] != NULL; i++){
+        if(str[i] != ' '){
+            count++;
+        }
+    }
+    printf("%d",count);
+}

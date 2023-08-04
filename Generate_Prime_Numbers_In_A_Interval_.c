ca4b1a14a95d930623b3f30c3fbaffebd61c8b0c
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-08-04 11:56:29 +0530

    #include<stdio.h> int isprime(int n){     if(n <= 1){         return 0;     }     else if(n == 2){         return 1;     }     else{         for(int i = 2; i <= n/2 + 1; i++){             if(n % i == 0){                 return 0;                 break;             }         }         return 1;     } } int main(){     int a,b;     scanf("%d%d",&a,&b);     for(int i = a; i <= b; i++){         if(isprime(i) == 1){             printf("%d ",i);         }     } }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
-int main()
-{
-    int a,b,i,j,count;
-    scanf("%d%d",&a,&b);
-    for(i=a+1;i<b;i++)
-    {
-        count=0;
-        for(j=2;j<i;j++)
-        {
-            if(i%j==0)
-            {
-                count=1;
+int isprime(int n){
+    if(n <= 1){
+        return 0;
+    }
+    else if(n == 2){
+        return 1;
+    }
+    else{
+        for(int i = 2; i <= n/2 + 1; i++){
+            if(n % i == 0){
+                return 0;
+                break;
             }
         }
-        if(count==0)
-        {
+        return 1;
+    }
+}
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i = a; i <= b; i++){
+        if(isprime(i) == 1){
             printf("%d
 ",i);
         }

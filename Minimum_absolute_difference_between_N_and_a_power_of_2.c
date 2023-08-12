cff436f250696e7f295e22ff84ed876011119bd6
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-08-12 19:25:00 +0530

    #include<stdio.h> int minimum(int a,int b){     if(a < b){         return a;     }     else{         return b;     } } int main(){     int n;     scanf("%d",&n);     int left = 1;     int right = 1;     while(right <= n){         right = right * 2;     }     left = right/2;     printf("%d",minimum((n - left),(right - n))); }

--- Minimum_absolute_difference_between_N_and_a_power_of_2.c
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
-int min(int a, int b){
-    if(a > b) return b;
-    else return a;
+int minimum(int a,int b){
+    if(a < b){
+        return a;
+    }
+    else{
+        return b;
+    }
 }
 int main(){
     int n;
     scanf("%d",&n);
+    int left = 1;
     int right = 1;
-    for(int i = 0; i < n; i++){
-        if(right <= n){
-            right *= 2;
-        }
+    while(right <= n){
+        right = right * 2;
     }
-    int left = right / 2;
-    printf("%d",min(n - left,right - n));
+    left = right/2;
+    printf("%d",minimum((n - left),(right - n)));
 }
